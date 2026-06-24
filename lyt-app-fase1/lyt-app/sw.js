a846145b9c567e4bcb3fb4e3f7fc6581e964d6fb
jlbustamante1969-web <jlbustamante1969@gmail.com>
2026-06-24 02:41:08 +0200

    Add files via upload

--- lyt-app-fase1/lyt-app/sw.js
+++ lyt-app-fase1/lyt-app/sw.js
@@ -1,27 +1,19 @@
-const CACHE = 'lyt-v3';
-const ASSETS = ['./', './index.html', './manifest.json'];
+var CACHE = 'lyt-v5';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch', function(e) {
+  // Siempre red primero, sin cache
+  e.respondWith(fetch(e.request).catch(function() { return caches.match(e.request); }));
 });
